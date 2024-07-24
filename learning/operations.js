0dc208d8a50f4fa422056e1e70b6f8dcf522af07
Abid Malik <malikabidzz0306@gmail.com>
2024-07-24 16:02:26 +0500

    learn-javascript

---
+++ learning/operations.js
@@ -0,0 +1,41 @@
+// console.log(2 + 2);
+
+// console.log(2 - 2);
+
+// console.log(2 * 2);
+
+// console.log(2 ** 3);
+
+// console.log(2 / 3);
+
+// console.log(2 % 3);
+
+
+let str1 = "Abid";
+
+let str2 = " ";
+
+let str3 = "Malik";
+
+let str4 = str1 + str2 + str3;
+
+// console.log(str4);
+
+
+// console.log(1 + 1);
+// console.log("1" + 1);
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+
+
