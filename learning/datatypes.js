b4946127e5e8279bc2b6390291aa6c0fec36e520
Abid Malik <malikabidzz0306@gmail.com>
2024-07-24 14:01:48 +0500

    learn-javascript

---
+++ learning/datatypes.js
@@ -0,0 +1,26 @@
+//Datatype conversion confusion
+
+let value = 22;
+ 
+console.log(typeof value);
+
+let checkNumber = Number(value);
+
+console.log(typeof checkNumber);
+
+console.log(checkNumber);
+
+
+let isLoggedIn = "1";
+
+let checkValue = Boolean(isLoggedIn);
+
+console.log(checkValue);
+
+
+let userName = 33;
+
+let checkValue1 = String(userName);
+
+console.log(checkValue1);
+console.log(typeof checkValue1)
