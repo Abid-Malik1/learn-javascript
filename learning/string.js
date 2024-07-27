05a1f3bf3ad4a09b5bbcdb3d87b18b3397c065a6
Abid Malik <malikabidzz0306@gmail.com>
2024-07-27 14:34:02 +0500

    string

--- learning/string.js
+++ learning/string.js
@@ -1,20 +1,27 @@
-let name = "Abid"
+// let name = "Abid"
 
-let myRepo = 80;
+// let myRepo = 80;
 
 
 // console.log("I " + name + " and my total git repo is " + myRepo);
 
 
-console.log(`Iam ${name} and my total git Repo is ${myRepo}`)
+// console.log(`Iam ${name} and my total git Repo is ${myRepo}`)
 
-const myName = "Abid Malik";
+// const myName = "Abid Malik";
 
-console.log(myName);
+// console.log(myName);
 
-const newName = new String("Abid Ali");
+// const newName = new String("Abid Ali");
 
-console.log(newName);
-console.log(newName[0]);
-console.log(newName[1]);
-console.log(newName[6]);
+// console.log(newName);
+// console.log(newName[0]);
+// console.log(newName[1]);
+// console.log(newName[6]);
+
+
+const url = "https://abimalik.com/generated%images";
+
+console.log(url.replace("%","$"));
+console.log(url.includes("malik1"));
+console.log(url.split(","))
